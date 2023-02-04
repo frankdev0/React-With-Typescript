d740b858274d19f0717cf0ddb3e32e5e9db2a0a4
Traders of Africa <nnaemeka@tradersofafrica.com>
2023-02-04 15:10:35 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import List from './components/List';
 
 function App() {
+
+  interface IState {
+    people: {
+      name:string,
+      age:number,
+      url:string,
+      note?:string
+    }[]
+  }
+
+
+
+  const [people, setPeople] = useState <IState["people"]>([
+    {
+      name: "Kobe Bryant",
+      age: 54,
+      url: "https://cdn.bleacherreport.net/images_root/slideshows/1044/slideshow_104463/default.jpg?x=973538",
+      note: "He is allergic to staying in one team for a year"
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List people={people}/>
+      <h1>This is where the Application starts</h1>
     </div>
   );
 }
